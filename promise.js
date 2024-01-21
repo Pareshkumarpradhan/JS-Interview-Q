46fcb6562d33f02ac3c6df524e9cd253d84bcd53
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2024-01-21 08:06:05 +0530

    promises question

---
+++ promise.js
@@ -0,0 +1,34 @@
+const cart = ["shose", "pants", "kurta"];
+
+//earlear solution
+// createOrder(cart, function (orderId) {
+
+//     proceedToPayment(orderId);
+
+// })
+
+// usinge promise
+// const promise = createOrder(cart);
+
+// promise.then(function (orderId) {
+
+//     proceedToPayment(orderId);
+
+// });
+
+
+// Q-> what is promise
+// A-> A promise is an object represnting the eventual complletion or faillure of an asynchronous operation
+
+
+// promise cheaning
+createOrder(cart)
+.then(function (orderId){
+  return proceedToPayment(orderId)
+})
+.then(function (paymentInfo) {
+   return showOrderSummary(paymentInfo);
+})
+.then(function(){
+   return updateWalletBalance()
+})
